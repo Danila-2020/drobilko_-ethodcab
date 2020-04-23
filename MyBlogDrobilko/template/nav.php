4018252011733dce8a57f0c81531363ecb763618
Danila-2020 <ddrobilko@mail.ru>
2020-04-23 16:07:06 +0300

    init3

---
+++ MyBlogDrobilko/template/nav.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg fixed-top">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-4">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Обо мне <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Портфолио</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Команда</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Блог</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакты</a>
+                </li>
+                <li><a href="#">
+                    <a class="nav-link" href="#">Зарегистрироваться</a>
+                </li>
+    
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
